615cd859b836d6d72e25757997c76a199983810d
LetianLi <letian_li@outlook.com>
2025-09-16 18:33:32 -0500

    comments

--- symDataloader/testConfig.py
+++ symDataloader/testConfig.py
@@ -3,7 +3,8 @@ Test configuration for TQA-Bench experiments.
 """
 
 # Database scales to test
-dbScales = ['8k']
+dbScales = ['128k']
+# dbScales = ['8k']
 
 # Table filter - only test these tables if array is not empty
 # If empty, test all tables
@@ -12,13 +13,15 @@ tableFilter = []
 
 # Context truncate length
 # If True, disables readTables tool, and truncates executePython output
-limitContextGrowth = True
+limitContextGrowth = False
+# limitContextGrowth = True
 
 # Inject context junk
 # If True, injects junk row into each table to fill up context.
 injectContextJunk = False
+# injectContextJunk = True # Remember to set limitContextGrowth to True if you use this
 
 # save file suffix
 saveFileSuffix = ''
-# saveFileSuffix = '_limited'
-# saveFileSuffix = '_limitedWithJunk'
+# saveFileSuffix = '_limited' # Use if limitContextGrowth is True
+# saveFileSuffix = '_limitedWithJunk' # Use if limitContextGrowth && injectContextJunk is True
